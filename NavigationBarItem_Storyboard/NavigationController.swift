44e18549916db51cf073b251dd51f42c51c97116
Seokhyun Kim <wjapp72@gmail.com>
2020-11-05 11:41:45 -0800

    #Add screenshot files

--- NavigationBarItem_Storyboard/NavigationController.swift
+++ NavigationBarItem_Storyboard/NavigationController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NavigationController.swift
 //  NavigationBarItem_Storyboard
 //
 //  Created by Seokhyun Kim on 2020-11-05.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
